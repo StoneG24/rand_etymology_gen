a49fac155772bf856a4ad1c47b8fb2365435197f
Stone Gorman <101292828+StoneG24@users.noreply.github.com>
2024-06-09 22:27:30 -0500

    Added comments to etymology.py

--- etymology.py
+++ etymology.py
@@ -4,8 +4,10 @@ import random
 filename = 'The_Oxford_3000.txt'
 randomnum = random.randint(0,3000)
 
-
+//chooses the random word
 line = random.choice(open(filename).readlines())
+
+//creates the url name
 url = "https://etymonline.com/word/%s" %line
 
 
